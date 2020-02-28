72c46d5fee56d6c9dadcac9f8c48b6c7b8a9d73b
Xiao Chuan <2017141461249@stu.scu.edu.cn>
2020-02-28 18:57:04 +0800

    del

--- lib/main.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Stu {
-    username: string;
-    password: string;
-    headers: object;
-    constructor(username: string, password: string);
-    login(): Promise<void>;
-    submit(geo_api_info: string): Promise<string>;
-    run(geo_api_info: string): Promise<void>;
-}
